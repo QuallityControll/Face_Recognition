d48abe8d5553bb25f0ba40f2393a652fe53b21bf
varmam1 <varmam@bxscience.edu>
2017-07-21 09:45:30 -0400

    Add files via upload

--- Descriptor Vectors Methods.py
+++ Descriptor Vectors Methods.py
@@ -3,13 +3,23 @@ import numpy as np
 from camera import take_picture
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from face_recog_database import update_descriptor, find_match
 
 load_dlib_models()
-
 from dlib_models import models
 
 
-def detect_faces_and_boxes():
+def detect_faces_and_boxes(person_database, tolerance=0.4):
+    """
+    This function detects the faces and displays the image with the boxes around the faces looking at the camera
+    while also putting their names if they are in the database and if not, it says not recognized
+
+    :param
+        person_database: Dictionary
+            This is the dictionary of names versus the descriptor vectors
+        tolerance:
+            A level of tolerance of the difference between the people.
+    """
 
     img_array = take_picture()
     fig, ax = plt.subplots()
@@ -22,41 +32,20 @@ def detect_faces_and_boxes():
     detections = face_detect(img_array, upscale)
     detections = list(detections)
 
-    print(len(detections))
-
-    for det in detections:
-        l, r, t, b = det.left(), det.right(), det.top(), det.bottom()
-        ax.add_patch(patches.Rectangle((l, t), np.abs(l - r), np.abs(t - b), fill=False))
-
-desc = []
-
-for i in range(2):
-    img_array = take_picture()
-    fig, ax = plt.subplots()
-    ax.imshow(img_array)
-
-    face_detect = models["face detect"]
-
-    # Number of times to upscale image before detecting faces.
-    # When would you want to increase this number?
-    upscale = 2
-
-    detections = face_detect(img_array, upscale)  # returns sequence of face-detections
-    detections = list(detections)
-
-    det = detections[0]  # first detected face in image
-
-    # bounding box dimensions for detection
-    l, r, t, b = det.left(), det.right(), det.top(), det.bottom()
-
     shape_predictor = models["shape predict"]
-    shape = shape_predictor(img_array, det)
-
     face_rec_model = models["face rec"]
 
-    # check that shape is (128,)
-    descriptor = np.array(face_rec_model.compute_face_descriptor(img_array, shape))
-    # print(descriptor)
-    desc.append(descriptor)
+    shapes = []
+    descriptors = []
+    names = []
 
-np.sqrt(np.sum((desc[0] - desc[1]) ** 2))  # L2 distance
+    for i in range(len(detections)):
+        shapes.append(shape_predictor(img_array, detections[i]))
+        descriptors.append(np.array(face_rec_model.compute_face_descriptor(img_array, shapes[i])))
+        names.append(find_match(person_database, descriptors[i], tolerance))
+
+    for i in range(len(detections)):
+        det = detections[i]
+        l, r, t, b = det.left(), det.right(), det.top(), det.bottom()
+        ax.add_patch(patches.Rectangle((l, t), np.abs(l - r), np.abs(t - b), fill=False))
+        ax.text(l, t, names[i], color="white")
